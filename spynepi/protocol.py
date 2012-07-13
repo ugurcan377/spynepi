301c57fce73a8a00bb0186f3c6d680c64e28200e
Ugurcan Ergun <ugurcanergn@gmail.com>
2012-07-13 10:12:29 +0300

    Fixed capitalization in custom protocol

--- spynepi/protocol.py
+++ spynepi/protocol.py
@@ -1,7 +1,7 @@
 
 from spyne.protocol.http import HttpRpc
 
-class SpynepiHttpRpc(HttpRpc):
+class SpynePiHttpRpc(HttpRpc):
     def decompose_incoming_envelope(self, ctx, message):
         HttpRpc.decompose_incoming_envelope(self, ctx, message)
 
