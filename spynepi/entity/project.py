c8ae2c1ed790c7e07e479f98a017d52f395e377b
Ugurcan Ergun <ugurcanergn@gmail.com>
2012-07-19 16:10:14 +0300

    deleted lxml hacks for doap.rdf after latest spyne patches they're no longer needed

--- spynepi/entity/project.py
+++ spynepi/entity/project.py
@@ -63,28 +63,3 @@ class RdfService(ServiceBase):
                 mbox=package.owners[0].person_email)),
             release=release_)
 
-
-def _on_method_return_document(ctx):
-    ctx.out_document = ctx.out_document[0]
-    ctx.out_document.tag = "{http://usefulinc.com/ns/doap#}Project"
-    ns0 = "{http://usefulinc.com/ns/doap#}"
-    ns1 = "{http://xmlns.com/foaf/0.1/}"
-    rdf = "{http://www.w3.org/1999/02/22-rdf-syntax-ns#}"
-    xml = ctx.out_document
-    xml.set(rdf+"about","/ornek")
-    homepage = xml.find(ns0+"homepage")
-    homepage.set(rdf+"resource","foo")
-    mbox = xml.xpath("//ns0:developer//ns1:Person//ns1:mbox",
-        namespaces={"ns0":"http://usefulinc.com/ns/doap#","ns1":"http://xmlns.com/foaf/0.1/"})
-    mbox[0].set(rdf+"resource","foo")
-    rlist = xml.findall(ns0+"release")
-    for element in rlist:
-        element.set(rdf+"about","spam")
-        temp = element.find(ns0+"Version")
-        fr = temp.find(ns0+"file-release")
-        if fr is not None:
-            fr.set(rdf+"resource","foo")
-#    print etree.tostring(xml,pretty_print=True)
-
-
-RdfService.event_manager.add_listener('method_return_document', _on_method_return_document)
